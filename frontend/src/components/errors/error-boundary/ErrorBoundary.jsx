e44f20e88d370f6b4270783cecfc7526ad4c6fa9
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-19 04:17:46 +0000

    set-up error boundary handling

---
+++ frontend/src/components/errors/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ErrorBoundaryStyled from "./ErrorBoundaryStyled";
+
+//components
+import ErrorBoundarySvg from "../../../assets/SVGs/ErrorBoundaryIcon";
+
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+    console.log(errorInfo);
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <ErrorBoundaryStyled>
+            <div className='errorWrapper'>
+            <div>
+                <ErrorBoundarySvg />
+                <br />
+                <strong>Oops! Something went wrong, we are fixing it</strong>
+            </div>
+            </div>
+        </ErrorBoundaryStyled>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
